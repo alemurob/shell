6c1c8e2d7a882028eb4d5b798ed8e567a18c801e
alemurob <robel.alemu@mail.utoronto.ca>
2025-10-08 14:28:39 -0400

    Made changes to resolve errors in Q5-7 with respect to file directories and simplifying file removal

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -56,8 +56,8 @@ cp -R ./data/raw/rawdata/event*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
-rm -I ./data/raw/rawdata/*ipaddr*
-rm -I ./data/processed/user_logs/*ipaddr*
+rm ./data/raw/rawdata/*ipaddr*
+rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
